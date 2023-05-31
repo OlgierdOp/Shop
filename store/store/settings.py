a339cef2965daa0234065805ed6b4aa4e8ea3ec7
OlgierdOp <you@example.com>
2023-05-31 18:09:08 +0200

    Commit 1

--- store/store/settings.py
+++ store/store/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-wj#es91*5)dwss@@pcyn+_9mxugvzr2)jejbmyfj6u1-)bli-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'shop123.herokuapp.com']
 
 # Application definition
 
